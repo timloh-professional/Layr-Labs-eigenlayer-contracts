aac2300fea53bf4d1dc92c76f12f4be92046d14f
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2023-08-01 17:28:24 -0700

    Add more functions to beacon chain proofs harness

--- src/test/harnesses/BeaconChainProofsHarness.sol
+++ src/test/harnesses/BeaconChainProofsHarness.sol
@@ -8,12 +8,54 @@ contract BeaconChainProofsHarness {
         uint40 validatorIndex,
         bytes32 beaconStateRoot,
         bytes calldata proof, 
-        bytes32[] calldata validatorFields) external view {        
+        bytes32[] calldata validatorFields) external view returns(bool) {        
             BeaconChainProofs.verifyValidatorFields(
                 validatorIndex,
                 beaconStateRoot,
                 proof, 
                 validatorFields
             );
-        }   
+            return true;
+    } 
+
+    function verifyValidatorBalance(
+        uint40 validatorIndex,
+        bytes32 beaconStateRoot,
+        bytes calldata proof,
+        bytes32 balanceRoot
+    ) external view returns(bool) {
+        BeaconChainProofs.verifyValidatorBalance(
+            validatorIndex,
+            beaconStateRoot,
+            proof,
+            balanceRoot
+        );
+        return true;
+    }
+
+    function verifySlotRoot(
+        bytes32 beaconStateRoot,
+        bytes calldata proof,
+        bytes32 slotRoot
+    ) external view returns(bool) {
+        BeaconChainProofs.verifySlotRoot(
+            beaconStateRoot,
+            proof,
+            slotRoot
+        );
+        return true;
+    }
+
+    function verifyStateRootAgainstLatestBlockHeaderRoot(
+        bytes32 beaconStateRoot,
+        bytes32 latestBlockHeaderRoot,
+        bytes calldata proof
+    ) external view returns(bool) {
+        BeaconChainProofs.verifyStateRootAgainstLatestBlockHeaderRoot(
+            beaconStateRoot,
+            latestBlockHeaderRoot,
+            proof
+        );
+        return true;
+    }   
 }
